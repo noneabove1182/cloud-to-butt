f80f40a71f7ebcf95d9a19ee68408e2a6b479bf8
noneabove1182 <ckealty1182@gmail.com>
2014-08-20 14:32:51 -0400

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bPudding\b/g, "Malenge");
+	v = v.replace(/\bpudding\b/g, "malenge");
 	
 	textNode.nodeValue = v;
 }
